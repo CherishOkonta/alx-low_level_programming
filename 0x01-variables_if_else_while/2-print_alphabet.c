c2731006c9d5ce1080149c89e0259280fc2af050
CherishOkonta <cherishokonta@gmail.com>
2022-12-12 20:48:01 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- prints alpha in lower case
+ *Return: return 0 always
+ */
+
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+putchar(lc);
+}
+putchar('\n');
+return (0);
+}

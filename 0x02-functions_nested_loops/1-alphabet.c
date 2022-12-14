248d09456b8930c48b416962782df3626819379c
CherishOkonta <cherishokonta@gmail.com>
2022-12-13 16:02:23 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  *print_alphabet- prints the alpha in lowercase
+ *Return: return 0
  */
 void print_alphabet(void)
 {
 char la;
 for (la = 'a'; la <= 'z'; la++)
-int _putchar(la)
+int _putchar(la);
 int _putchar('\n');
 }

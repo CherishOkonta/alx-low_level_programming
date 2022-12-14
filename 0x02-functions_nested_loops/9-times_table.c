889bece8b2c4794238acf0295b8784cf41265f4d
CherishOkonta <cherishokonta@gmail.com>
2022-12-13 19:41:49 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,20 +5,37 @@
  */
 void times_table(void)
 {
-int num, mult, prod;
+int a = 0;
+int b;
+int rep;
+while (a <= 9)
 {
-_putchar('0');
-for (mult = 1; mult <= 9; mult++)
+b = 0;
+while (b <= 9)
+{
+rep = a * b;
+if (b == 0)
+{
+_putchar('0' + rep);
+}
+else if (rep < 10)
+{
+_putchar(' ');
+_putchar('0' + rep);
+}
+else
+{
+_putchar('0' + rep / 10);
+_putchar('0' + rep % 10);
+}
+if (b < 9)
 {
 _putchar(',');
 _putchar(' ');
-prod = num + mult;
-if (prod <= 9)
-_putchar(' ');
-else
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
+}
+b++;
 }
 _putchar('\n');
+a++;
 }
 }

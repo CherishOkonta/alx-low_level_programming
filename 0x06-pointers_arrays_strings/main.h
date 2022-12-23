b87a3bf237d2c7caf40ff4dcbf054c3a0fe60212
CherishOkonta <cherishokonta@gmail.com>
2022-12-22 19:33:02 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+/**
+*void prototypes()- these are void prototypes
+*int prototypes(): void prototypes
+*char prototypes(): void char prototypes
+*@n: where n is integer
+*/
+int main(void);
+void print_number(int n);
+void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *a);
-char *cap_string(char *a);
-char *leet(char *a);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 int _putchar(char c);
 
 #endif

f3959602b415925eec6eacda7ef1e0d692a83cc5
CherishOkonta <cherishokonta@gmail.com>
2023-01-03 12:25:59 -0800

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
+void *_memset(void *s, int c, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *strchr(char *s, char c);
+unsigned int_strson(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);

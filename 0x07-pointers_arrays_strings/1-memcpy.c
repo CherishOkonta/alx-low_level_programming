77808dfcc20431da4aaaeca9be7ee56787c544ad
CherishOkonta <cherishokonta@gmail.com>
2023-01-03 12:59:20 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,27 +1,18 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
+ * *_memcpy -copies memory area from src to dest
+ * @dest: destination
+ * @src: source
+ * @n: max bytes to use
+ * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
-int j;
-i = 0;
-while (dest[i] != '\0')
+unsigned int i;
+for (i = 0; n > 0 ; i++, n--)
 {
-i++;
+dest[i] = src[i];
 }
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
 return (dest);
 }
